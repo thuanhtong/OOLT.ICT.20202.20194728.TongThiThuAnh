5a3324185ea659ef5e2452dee1e2db061bd595a0
thuanhtong <xxxthuanhxxx@gmail.com>
2021-03-14 21:02:48 +0700

    commit Lab03

---
+++ Lab03/src/Cart.java
@@ -0,0 +1,41 @@
+
+public class Cart {
+	public static final int MAX_NUMBERS_ORDERED =20;
+	
+	private DigitalVideoDisc itemOrdered[]= new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	
+	private int qtyOrdered=0;
+
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+		if(qtyOrdered <20) {
+			itemOrdered[qtyOrdered]= disc;
+			System.out.println("The disc '" + disc.getTitle() + "' has been added.");
+			qtyOrdered++;
+			if(qtyOrdered == 19) System.out.println("The cart is almost full.");
+			if(qtyOrdered == 20) System.out.println("The cart is full.");
+		}
+		else System.out.println("Can't add the disc '" + disc.getTitle() + "' into the cart. The cart is already full!");
+	}
+	
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+		int flag=0;
+		for(int i=0; i<qtyOrdered; i++) {
+			if (itemOrdered[i] == disc) {
+				itemOrdered[i]= itemOrdered[qtyOrdered-1];
+				qtyOrdered--;
+				System.out.println("The disc '" + disc.getTitle()+ "' has been removed from the cart.");
+				flag++;
+			}
+		}
+		if(flag==0) System.out.println("The disc '" + disc.getTitle() + "' isn't in the cart.");
+	}
+	
+	public float totalCost() {
+		float sumCost=0;
+		for(int i=0; i<qtyOrdered; i++) {
+			sumCost+= itemOrdered[i].getCost();
+		}
+		return sumCost;
+	}
+}
+
--- Lab03/src/DigitalVideoDisc.java
+++ Lab03/src/DigitalVideoDisc.java
@@ -2,7 +2,7 @@
 public class DigitalVideoDisc {
 	private String title;
 	private String category;
-	private String directory;
+	private String director;
 	private int length;
 	private float cost;
 	
@@ -13,8 +13,8 @@ public class DigitalVideoDisc {
 	public String getCategory() {
 		return category;
 	}
-	public String getDirectory() {
-		return directory;
+	public String getDirector() {
+		return director;
 	}
 	public int getLength() {
 		return length;
@@ -28,6 +28,18 @@ public class DigitalVideoDisc {
 		super();
 		this.title = title;
 	}
+//	public DigitalVideoDisc(String category) {
+//		super();
+//		this.category = category;
+//	}
+	
+	//Question: If you create a constructor method to build a DVD by title then create a constructor
+	//method to build a DVD by category. Does JAVA allow you to do this?
+	//Answer: JAVA doesn't allow us to do this, it will report an error of Duplicate method of 
+	//DigitalVideoDisc(String) in type DigitalVideoDisc. Because both Title and Category are String field,
+	//if we call DigitalVideoDisc("A_String") for a string, the program can't distinguish whether 
+	//the string is a name or a category.
+
 	//Create a DVD object by category, title and cost
 	public DigitalVideoDisc(String title, String category, float cost) {
 		super();
@@ -36,21 +48,22 @@ public class DigitalVideoDisc {
 		this.cost = cost;
 	}
 	//Create a DVD object by director, category, title and cost
-	public DigitalVideoDisc(String title, String category, String directory, float cost) {
+	public DigitalVideoDisc(String title, String category, String director, float cost) {
 		super();
 		this.title = title;
 		this.category = category;
-		this.directory = directory;
+		this.director = director;
 		this.cost = cost;
 	}
 	//Create a DVD object by all attributes: title, category, director, length and cost
-	public DigitalVideoDisc(String title, String category, String directory, int length, float cost) {
+	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
 		super();
 		this.title = title;
 		this.category = category;
-		this.directory = directory;
+		this.director = director;
 		this.length = length;
 		this.cost = cost;
 	}
 
 }
+
